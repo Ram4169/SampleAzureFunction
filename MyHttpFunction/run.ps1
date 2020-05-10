375fccac1eec9ff09d5a86a2834dfbe656268819
Ram4169 <rana786sharma@gmail.com>
2020-05-11 00:18:32 +0530

    Updated

--- MyHttpFunction/run.ps1
+++ MyHttpFunction/run.ps1
@@ -28,5 +28,5 @@ $employees = Get-Content .\database\employees.json | ConvertFrom-Json
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     headers = @{'content-type'='application\json'}
     StatusCode = [httpstatuscode]::OK
-    Body = $employees
+    Body = $employees.employees
 })
