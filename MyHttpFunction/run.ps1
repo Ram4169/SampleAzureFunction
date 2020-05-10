98b623e62a9e97f0e688e8b3bf9993f70ec2bafc
Ram4169 <rana786sharma@gmail.com>
2020-05-10 20:49:15 +0530

    Updated

--- MyHttpFunction/run.ps1
+++ MyHttpFunction/run.ps1
@@ -3,23 +3,30 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+# # Write to the Azure Functions log stream.
+# Write-Host "PowerShell HTTP trigger function processed a request."
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
+# # Interact with query parameters or the body of the request.
+# $name = $Request.Query.Name
+# if (-not $name) {
+#     $name = $Request.Body.Name
+# }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+# $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
-}
+# if ($name) {
+#     $body = "Hello, $name. This HTTP triggered function executed successfully."
+# }
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
+# # Associate values to output bindings by calling 'Push-OutputBinding'.
+# Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+#     StatusCode = [HttpStatusCode]::OK
+#     Body = $body
+# })
+
+$employees = Get-Content .\database\employees.json | ConvertFrom-Json
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    headers = @{'content-type'='application\json'}
+    StatusCode = [httpstatuscode]::OK
+    Body = $employees
 })
