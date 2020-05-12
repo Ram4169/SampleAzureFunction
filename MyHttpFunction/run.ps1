1ec2d6c666cefb77476e8b72501cc8909cf7b0a0
Ram4169 <rana786sharma@gmail.com>
2020-05-12 14:18:40 +0530

    updated

--- MyHttpFunction/run.ps1
+++ MyHttpFunction/run.ps1
@@ -26,7 +26,7 @@ param($Request, $TriggerMetadata)
 
 $employees = Get-Content .\database\employees.json | ConvertFrom-Json
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    #headers = @{'content-type'='application\json'}
-    StatusCode = [httpstatuscode]::OK
-    Body = $employees.employees
+    headers = @{'content-type'='application\json'}
+    StatusCode = [HttpStatusCode]::OK
+    Body = $employees
 })
